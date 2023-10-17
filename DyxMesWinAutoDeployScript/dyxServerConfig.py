8c918fd2d3285e6be73ef74b507d44d09a07d446
Smile-a <1020229033@qq.com>
2023-10-17 18:26:21 +0800

    完善了winscp功能

--- DyxMesWinAutoDeployScript/dyxServerConfig.py
+++ DyxMesWinAutoDeployScript/dyxServerConfig.py
@@ -1,5 +1,21 @@
+#登录用户名称
+loginUserName = "anwei"
+#winscp软件窗口标题
+winsctTitle = "@10.157.234.151 – WinSCP"
+#前台页面打包好的存放路径
+frontendPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxmes-frontend\\build\\"
+#前台打包好的zip
+frontendZipName = "dist-pc.zip"
+#jar包存放的路径
+jarFilesPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\"
+jar_system = jarFilesPath + "hbtobacco-dyxmes-parent\hbtobacco-dyxmes-system\\target"
+jar_getway = jarFilesPath + "hbtobacco-dyxmes-parent\hbtobacco-dyxmes-getway\\target"
+jar_quality = jarFilesPath + "hbtobacco-dyxmes-parent\hbtobacco-dyxmes-quality\\target"
+jar_plan = jarFilesPath + "hbtobacco-dyxmes-parent\hbtobacco-dyxmes-plan\\target"
+jar_base = jarFilesPath + "hbtobacco-dyxmes-parent\hbtobacco-dyxmes-base\\target"
+jar_warehouse = jarFilesPath + "hbtobacco-dyxmes-parent\hbtobacco-dyxmes-warehouse\\target"
 #测试服务器前台页面
 test_10_156_53_58 = { 
-    "Xshell" : "eyJkZXZfaWQiOiI3NjMiLCJwd2QiOnsidXNyX2VwdyI6IkdJM1RYSGl1bER5UzlOMm4lMkJGNUdMdzdLNk5XbmxIUGdaM20xNTY1NzFEMCUzRCIsImhvc3QiOiIxMC4xNTcuMjM0LjE1MSIsInVzZXJuYW1lIjoiYW53ZWlAJCRANzYzIn0sImRldl9pcCI6IjEwLjE1Ni41My41OCIsImFwcF9uYW1lMiI6InNzaCIsImRldl9lbmNvZGluZyI6IlVURi04IiwiYXBwX3BvcnQiOiIyMiIsImFjY3RfbmFtZSI6InJvb3QiLCJsb2dpblVzZXJOYW1lIjoiYW53ZWkiLCJTRVJWRVJfQUREUiI6IjEwLjE1Ny4yMzQuMTUxIiwidHlwZSI6IlhzaGVsbCIsInBvcnQiOiIyMiJ9",
-    "winscp" : "eyJkZXZfaWQiOiI3NjMiLCJwd2QiOnsidXNyX2VwdyI6IkdJM1RYSGl1bER5UzlOMm4lMkJGNUdMdzdLNk5XbmxIUGdaM20xNTY1NzFEMCUzRCIsImhvc3QiOiIxMC4xNTcuMjM0LjE1MSIsInVzZXJuYW1lIjoiYW53ZWlAJCRANzYzIn0sImRldl9pcCI6IjEwLjE1Ni41My41OCIsImFwcF9uYW1lMiI6InNmdHAiLCJkZXZfZW5jb2RpbmciOiJVVEYtOCIsImFwcF9wb3J0IjoiMjIiLCJhY2N0X25hbWUiOiJyb290IiwibG9naW5Vc2VyTmFtZSI6ImFud2VpIiwiU0VSVkVSX0FERFIiOiIxMC4xNTcuMjM0LjE1MSIsInR5cGUiOiJ3aW5zY3AiLCJwb3J0IjoiMjIiLCJhY2N0X2lkIjoiOTA5In0="
+    "Xshell" : "eyJkZXZfaWQiOiI3NjMiLCJwd2QiOnsidXNyX2VwdyI6InlZdlJiTm90Y0lLdzZXamZ5TDh2SGxLVXItNVFlU2RxVG5hZEpFT0JjSkklM0QiLCJob3N0IjoiMTAuMTU3LjIzNC4xNTEiLCJ1c2VybmFtZSI6ImFud2VpQCQkQDc2MyJ9LCJkZXZfaXAiOiIxMC4xNTYuNTMuNTgiLCJhcHBfbmFtZTIiOiJzc2giLCJkZXZfZW5jb2RpbmciOiJVVEYtOCIsImFwcF9wb3J0IjoiMjIiLCJhY2N0X25hbWUiOiJyb290IiwibG9naW5Vc2VyTmFtZSI6ImFud2VpIiwiU0VSVkVSX0FERFIiOiIxMC4xNTcuMjM0LjE1MSIsInR5cGUiOiJYc2hlbGwiLCJwb3J0IjoiMjIifQ==",
+    "winscp" : "eyJkZXZfaWQiOiI3NjMiLCJwd2QiOnsidXNyX2VwdyI6Ik9HRnMtOVAlMkJyVkN3YTQ3dEJuMlB0QVktcTJRZlM0SEllMEpkSDRPRm44WSUzRCIsImhvc3QiOiIxMC4xNTcuMjM0LjE1MSIsInVzZXJuYW1lIjoiYW53ZWlAJCRANzYzIn0sImRldl9pcCI6IjEwLjE1Ni41My41OCIsImFwcF9uYW1lMiI6InNmdHAiLCJkZXZfZW5jb2RpbmciOiJVVEYtOCIsImFwcF9wb3J0IjoiMjIiLCJhY2N0X25hbWUiOiJyb290IiwibG9naW5Vc2VyTmFtZSI6ImFud2VpIiwiU0VSVkVSX0FERFIiOiIxMC4xNTcuMjM0LjE1MSIsInR5cGUiOiJ3aW5zY3AiLCJwb3J0IjoiMjIiLCJhY2N0X2lkIjoiOTA5In0="
 }
