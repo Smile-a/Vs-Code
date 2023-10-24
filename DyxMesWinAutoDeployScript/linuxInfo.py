c3b8f4651d4fca494702c3af0be0d0c13bab8f0a
Smile-a <1020229033@qq.com>
2023-10-24 18:23:49 +0800

    emm

--- DyxMesWinAutoDeployScript/linuxInfo.py
+++ DyxMesWinAutoDeployScript/linuxInfo.py
@@ -17,9 +17,8 @@ def get_local_ip():
 
 if __name__=='__main__':
     while(True):
-        sleep(1)
         free = get_sys_info()
         if(free > 95):
-            #发送邮件通知
             print("不好辣，服务器内存不够了~",get_local_ip())
+            #发送邮件通知
             break
