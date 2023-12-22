9eddf7d4bcb2e41a99d0c1df10431285a74dd712
Smile-a <1020229033@qq.com>
2023-12-22 11:22:42 +0800

    干掉写死的参数

--- dyx3D/compAuto.py
+++ dyx3D/compAuto.py
@@ -4,38 +4,8 @@ import socket
 import paramiko
 import compAutoConfig
 
-
-#是否上传开关
-isFrontend = True
-isSystem = True
-isKanban = True
-#路径配置
-ipAddr = "192.168.42.127"
-userName = "root"
-password = "whlinux"
-#前台项目文件夹地址--懒得在脚本里面svn update
-frontendDirPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxvisualization-frontend\\"
-#前台页面打包脚本路径
-frontendBuildPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxvisualization-frontend\\build\\"
-frontendBatName = "build.bat"
-#后台打包脚本路径
-jarPackBuildPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxvisualization-parent\\"
-#在调用这个bat的时候先去把他最后一行的cmd /k注释掉，不然会卡住
-jarBatCompName = "打包-公司环境.bat"
-#具体上传到服务器的文件
-frontendPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxvisualization-frontend\\build\dist-pc.zip"
-systemJarPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxvisualization-parent\\hbtobacco-dyxvisualization-system\\target\\hbtobacco-dyxvisualization-system.jar"
-kanbanJarPath = "C:\\Users\\wsd\\eclipse-workspace\\dyxmesProject\\hbtobacco-dyxvisualization-parent\\hbtobacco-dyxvisualization-kanban\\target\\hbtobacco-dyxvisualization-kanban.jar"
-#上传jar包和zip文件到服务器配置
-FrontendlinuxPath = '/home/dyxvisual/dist-pc.zip'
-SystemlinuxPath = '/root/hbtobacco-dyxvisualization-system.jar'
-KanbanlinuxPath = '/root/hbtobacco-dyxvisualization-kanban.jar'
-#ssh命令
-cdStr = 'cd /home/dyxvisual/; '
-
 #3d可视化到开发环境打包
 if(__name__=="__main__"):
-    
     #多此一举的操作
     pyFilePath = sys.path[0]
     print("程序路径:",pyFilePath)
