25a7b444a2ca693813b1985657c3cb9b2814aba1
Smile-a <1020229033@qq.com>
2023-12-22 17:15:53 +0800

    彻底改造，打成exe不太好取我的配置文件，改成json文件算了，同级目录直接拿。简单粗暴

--- dyx3D/compAuto.py
+++ dyx3D/compAuto.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import json
 import socket
 import paramiko
-import compAutoConfig
+#import compAutoConfig
 
 # 打包成exe基础命令就够了，默认会打包成文件夹
 # pyinstaller -D -c -i naotou.ico compAuto.py 文件夹
@@ -19,6 +20,11 @@ if(__name__=="__main__"):
     print("【淡雅香3D可视化项目公司环境自动打包脚本】")
     print("可能会较慢，因为是从本地上传的奥~")
     
+    # 读取文件内容  
+    with open('compAutoConfig.json', 'r' ,encoding="utf-8") as file:
+        jsonFileData = json.load(file)
+        print(jsonFileData)
+    
     #如果有想法的话可以自己改造，通过命令行输入的参数来启动对应的模块
     # @echo off  
     # python "C:\\MyProject\\Vs-Code\\dyx3D\\compAuto.py" %* kanabn
@@ -28,19 +34,19 @@ if(__name__=="__main__"):
     
     #切换目录,打包前台项目
     print("开始更新前台项目...")
-    os.chdir(compAutoConfig.frontendDirPath)
+    os.chdir(jsonFileData['frontendDirPath'])
     os.system("svn update")
     print("前台项目svn更新完毕...")
     
     print("开始打包前台项目...")
-    os.chdir(compAutoConfig.frontendBuildPath)
-    os.system(compAutoConfig.frontendBatName)
+    os.chdir(jsonFileData['frontendBuildPath'])
+    os.system(jsonFileData['frontendBatName'])
     print("前台项目打包完成！")
     
     #切换目录,打包后台项目
     print("开始打包后台项目...")
-    os.chdir(compAutoConfig.jarPackBuildPath)
-    os.system(compAutoConfig.jarBatCompName)
+    os.chdir(jsonFileData['jarPackBuildPath'])
+    os.system(jsonFileData['jarBatCompName'])
     print("后台项目打包完成!")
     
     #切换回来--py文件没问题，exe打包成单独文件就不可以看了，因为是zip
@@ -49,54 +55,54 @@ if(__name__=="__main__"):
     
     # 创建一个套接字对象，连接到远程服务器  
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    sock.connect((compAutoConfig.ipAddr, 22))  
+    sock.connect((jsonFileData['ipAddr'], 22))  
     
     # 创建SSHClient对象，并设置连接参数  
     ssh = paramiko.SSHClient()  
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  
     
     # 连接服务器  
-    ssh.connect(hostname=compAutoConfig.ipAddr, username=compAutoConfig.userName, password=compAutoConfig.password, sock=sock) 
+    ssh.connect(hostname=jsonFileData['ipAddr'], username=jsonFileData['userName'], password=jsonFileData['password'], sock=sock) 
     
     # 创建SFTPClient对象，并传入SSH连接  
     sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())  
     
-    if compAutoConfig.isFrontend:
+    if jsonFileData['isFrontend']:
         #上传文件
         print("链接服务器成功，开始上传dist-pc.zip,请等待...")
-        result = sftp.put(compAutoConfig.frontendPath, compAutoConfig.FrontendlinuxPath)
+        result = sftp.put(jsonFileData['frontendPath'], jsonFileData['FrontendlinuxPath'])
         if result:
             print("前端zip上传成功!")
             #执行命令
-            stdin, stdout, stderr = ssh.exec_command(compAutoConfig.cdStr + ' unzip -o dist-pc.zip')
+            stdin, stdout, stderr = ssh.exec_command(jsonFileData['cdStr'] + ' unzip -o dist-pc.zip')
             print(stdout.read().decode())
         else:
             print("前端上传失败!")
     
-    if compAutoConfig.isSystem:
+    if jsonFileData['isSystem']:
         #上传文件
         print("链接服务器成功，开始上传system.jar,请等待...")
-        result = sftp.put(compAutoConfig.systemJarPath, compAutoConfig.SystemlinuxPath)
+        result = sftp.put(jsonFileData['systemJarPath'], jsonFileData['SystemlinuxPath'])
         if result:
             print("system上传成功!")
             #执行命令
-            pwdi,pwdo,pwde = ssh.exec_command(compAutoConfig.cdStr + ' pwd')
+            pwdi,pwdo,pwde = ssh.exec_command(jsonFileData['cdStr'] + ' pwd')
             print(pwdo.read().decode())
-            stdin, stdout, stderr = ssh.exec_command(compAutoConfig.cdStr + ' sh deploy-dyxvisual.sh system_start')
+            stdin, stdout, stderr = ssh.exec_command(jsonFileData['cdStr'] + ' sh deploy-dyxvisual.sh system_start')
             print(stdout.read().decode())
         else:
             print("system上传失败!")
     
-    if compAutoConfig.isKanban:
+    if jsonFileData['isKanban']:
         #上传文件
         print("链接服务器成功，开始上传kanban.jar,请等待...")
-        result = sftp.put(compAutoConfig.kanbanJarPath, compAutoConfig.KanbanlinuxPath)
+        result = sftp.put(jsonFileData['kanbanJarPath'], jsonFileData['KanbanlinuxPath'])
         if result:
             print("kanban上传成功!")
             #执行命令.好坑!就是execute_command() 他是a single session，每次执行完后都要回到缺省目录。所以可以 .execute_command('cd  /var; pwd')
-            pwdi,pwdo,pwde = ssh.exec_command(compAutoConfig.cdStr + ' pwd')
+            pwdi,pwdo,pwde = ssh.exec_command(jsonFileData['cdStr'] + ' pwd')
             print(pwdo.read().decode())
-            stdin, stdout, stderr = ssh.exec_command(compAutoConfig.cdStr + ' sh deploy-dyxvisual.sh kanban_start')
+            stdin, stdout, stderr = ssh.exec_command(jsonFileData['cdStr'] + ' sh deploy-dyxvisual.sh kanban_start')
             print(stdout.read().decode())
         else:
             print("kanban上传失败!")
