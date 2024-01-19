e40595040b4dbdaab000c9e0f87155d2ccc08fc1
Smile-a <1020229033@qq.com>
2024-01-19 16:09:35 +0800

    优化一下svn检测

--- svnDeme/svnCheckUpFile.py
+++ svnDeme/svnCheckUpFile.py
@@ -1,30 +1,19 @@
 import os
-
-# 创建一个空的集合  
-my_set = set()
-# 添加一些字符串到集合中  
-my_set.add("connection.properties")
-my_set.add("application.properties")
-my_set.add("wsdJedisConfig.properties")
-my_set.add("server.properties")
-my_set.add("logback-spring.xml")
-my_set.add("Dockerfile")
-my_set.add("gen.java")
-my_set.add("GEN.java")
-my_set.add("genQuality.java")
-my_set.add("pom.xml")
-my_set.add("Test.java")
-my_set.add("hbtobacco-dyxmes-web")
-my_set.add("build.bat")
-my_set.add("dyxvisuali-nginx.conf")
-my_set.add("output.txt")
+import json
 
 #启动！
-if __name__=='__main__': 
-    path = 'C:\WisdomInfo\IdeaWorkspace\svn\ERD预销售库\ERD_PROJ\java_wh_2021\hbprojects\hbzy\dyxmesProject'
+if __name__=='__main__':
+    #程序根路径
+    rootPath = os.getcwd()
+    print(rootPath)
+    # 读取 JSON 文件  
+    with open(rootPath+'\\verificationRejection.json', 'r',encoding="utf-8") as file:
+        jsonData = json.load(file)
+    #我要检测的项目svn目录
+    path = jsonData['svnProjectPath']
     os.chdir(path)
-    print("当前路径",os.getcwd())
-    print("开始检测当前目录下svn文件状态")
+    print("路径已切换，当前路径",os.getcwd())
+    print("开始检测当前目录下svn文件状态,输出至output.txt")
     os.system("svn status --show-updates > output.txt")
     #打开文件
     with open('output.txt', 'r') as file:
@@ -38,12 +27,11 @@ if __name__=='__main__':
             if index != -1:
                 # 截取反斜杠及其后面的内容  
                 substring = strLine[index + 1:]
-                #print(substring)
-                if substring in my_set:
+                if substring in jsonData:
                     #print("字符串已存在于集合中")
                     continue
                 else:
                     print(substring)
             #else:
                 #print(strLine)
-    print("检测完毕，以上是当前目录中可能需要提交的文件信息。")
+    print("检测完毕，以上是当前目录中可能需要提交或更新的文件信息。")
