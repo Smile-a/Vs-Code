365a45c20cf1e5c63f187190e3cb729b0741613a
Smile-a <1020229033@qq.com>
2024-02-28 10:05:06 +0800

    鼠标定位脚本完成

--- pyDemo/movoDemo.py
+++ pyDemo/movoDemo.py
@@ -3,8 +3,13 @@ import time
 
 #启动！
 if __name__=='__main__':
+    sleepTime = 0.5
     #初始坐标
     previous_x, previous_y = pyautogui.position()
+    print("hello 请输入你需要的触发间隔时间，回车确认,默认是0.5s噢！")
+    sleepTime_input = input()  # 用户输入触发间隔时间
+    if sleepTime_input:  # 如果用户有输入则更新sleepTime
+        sleepTime = float(sleepTime_input)
     #一直找
     while True:    
         current_x, current_y = pyautogui.position()
@@ -14,4 +19,4 @@ if __name__=='__main__':
         print('鼠标位置变化：x = {}, y = {}'.format(diff_x, diff_y))
         previous_x, previous_y = current_x, current_y
         print("###############################")
-        time.sleep(3)
+        time.sleep(sleepTime)
