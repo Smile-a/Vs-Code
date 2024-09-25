716f962b36c4aa36ac8692dbb7e3385aeba6cf41
anwei <1020229033@qq.com>
2024-09-25 15:08:43 +0800

    demo

--- pyDemo/redisDemo.py
+++ pyDemo/redisDemo.py
@@ -1,17 +1,32 @@
 import redis
 
 # 连接redis 设置存储的数据库为14 将从Redis服务器接收到的值默认为字符串类型
-r = redis.Redis(host='localhost', port=6379, db=14 , decode_responses=True)
+r = redis.Redis(host='localhost', port=6379, db=5 , decode_responses=True)
 
 # 清空list数据
-r.delete('list')
+# r.delete('list')
 
 # 设置list值，字符串类型
-r.rpush('list', '1')
-r.rpush('list', '2')
+# r.rpush('list', '1')
+# r.rpush('list', '2')
 
 #获取list总条数
-print(r.llen('list'))
+# print(r.llen('list'))
 
 # 获取list值
-print(r.lrange('list', 0, -1))
+# print(r.lrange('list', 0, -1))
+
+#获取当前redis数据库所有的元素列表
+#print(r.keys())
+#把所有元素的key写入到successList集合中
+# for i in r.keys():
+#     r.sadd('successList', i)
+
+# 删除集合中的所有元素，除了successList
+# for i in r.keys():
+#     if i != 'successList':
+#         r.delete(i)
+
+r.delete('excelErrList')
+r.delete('webDataNullList')
+r.delete('TpList')
