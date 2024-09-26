ab5c29ee1d9f315c36cf20a00576fab2e26aa5dc
anwei <1020229033@qq.com>
2024-09-26 14:06:22 +0800

    修改判断条件

--- dataModelAuto/Main.py
+++ dataModelAuto/Main.py
@@ -111,7 +111,7 @@ def automationBegins():
         # 模型英文名称
         mxCode = sheetName[0]
         # 判断这个sheet页模型Code是否已经在redis中
-        if r.sismember('successList', mxCode) or r.sismember('TpList', mxCode) or r.sismember('excelErrList', mxCode) or r.sismember('webDataNullList', mxCode):
+        if r.sismember('successList', mxCode) or r.sismember('TpList', sheet.name) or r.sismember('excelErrList', sheet.name) or r.sismember('webDataNullList', sheet.name):
             print("模型"+mxCode+"已经存在Redis中，跳过该项")
             continue
         # 模型中文名称
